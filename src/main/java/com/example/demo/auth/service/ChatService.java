64ae79a82dc40c71851aca5d7e637e4e113d78c3
moonseongmin97 <wja04097@naver.com>
2024-12-05 17:43:06 +0900

    채팅 설정하는거

--- src/main/java/com/example/demo/auth/service/ChatService.java
+++ src/main/java/com/example/demo/auth/service/ChatService.java
@@ -2,6 +2,7 @@ package com.example.demo.auth.service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -59,10 +60,11 @@ public class ChatService {
             }else {
                 	chatRepository.setNickName(chatUserEntity);
                 	System.out.println("결과값은???===="+chatUserEntity);
+                	ChatUserResponseDto responseDto = modelMapper.map(requestDto, ChatUserResponseDto.class);
                 	//레디스에 닉네임 저장.                
-                	Map<String,Object> response = loginUtil.updateNicknameInRedis(requestDto.getUuid().toString(),"nickName" ,chatUserEntity.getNickName());
+                	//Map<String,Object> response = loginUtil.updateNicknameInRedis(requestDto.getUuid().toString(),"nickName" ,chatUserEntity.getNickName());
                 	
-                	result.put("data", response);
+                	result.put("data", responseDto);
                     result.put("state", true);
                     result.put("msg", "닉네임 설정 완료.");   
             }
@@ -74,6 +76,49 @@ public class ChatService {
         }
         return result;
     }
+    
+    
+
+    /**
+     * 채팅 닉네임 조회
+     * @param requestDto 회원 가입 정보를 담은 DTO
+     * @return Map<String, Object> 상태 메시지를 담은 결과
+     */
+    public Map<String, Object> getNickName(MemberRequestDto requestDto) {
+        Map<String, Object> result = new HashMap<>();
+        
+        try {
+        	if(requestDto.getUuid()!=null) {
+	        	ChatUserEntity chatUserEntity = modelMapper.map(requestDto, ChatUserEntity.class);
+	        	 ChatUserResponseDto responseDto = new ChatUserResponseDto();
+	            // 채팅 중복 확인            
+	            Optional<ChatUserEntity> entityResult = chatRepository.getNickName(chatUserEntity.getUserId());
+	            System.out.println("결과===="+entityResult.isPresent());
+	            if(entityResult.isPresent()) {
+	             responseDto = modelMapper.map(entityResult.get(), ChatUserResponseDto.class);
+	     			result.put("data", responseDto);
+	     			result.put("state", true);
+	     			result.put("msg", "닉네임 체크 완료.");  
+	            }else {
+	            	responseDto = modelMapper.map(requestDto, ChatUserResponseDto.class);		
+	            		result.put("data", responseDto);
+	                    result.put("state", true);
+	                    result.put("msg", "로그인은 했지만 닉네임 조회 결과 없다잉.");
+	            }
+        	}else {        	
+                result.put("state", true);
+                result.put("msg", "비회원 사용자");
+        	}
+            
+        } catch (Exception e) {
+            result.put("state", false);
+            result.put("msg", "닉네임 설정중 오류 발생");
+            result.put("error", e.getMessage());
+            e.printStackTrace();
+        }
+        return result;
+    }
+    
     
     
 }
